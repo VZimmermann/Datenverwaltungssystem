8ce364a34154ae34d894dd6ac5f8bc748bf306f8
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-07-15 08:43:17 +0200

    Create PropertyException.java

---
+++ src/main/java/org.intercard.template.ex/PropertyException.java
@@ -0,0 +1,26 @@
+package org.intercard.template.ex;
+
+public class PropertyExeption extends ServerExeption {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2630786283570827685L;
+
+	public PropertyExeption() {
+		super();
+	}
+
+	public PropertyExeption(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PropertyExeption(String message) {
+		super(message);
+	}
+
+	public PropertyExeption(Throwable cause) {
+		super(cause);
+	}
+
+}
