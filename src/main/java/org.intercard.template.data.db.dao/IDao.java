3154384ef8511d5101d3866f28ee024c4757a84b
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-07-15 08:20:18 +0200

    Create IDao.java

---
+++ src/main/java/org.intercard.template.data.db.dao/IDao.java
@@ -0,0 +1,21 @@
+package org.intercard.template.data.db.dao;
+
+import java.util.List;
+
+import org.intercard.template.data.db.ex.DataException;
+
+public interface IDao<T> {
+
+	// -------------------------------------------------------------------------
+	// create, save, update, delete by Object
+	// -------------------------------------------------------------------------
+	public T findOne(Long id) throws DataException;
+
+	public List<T> findAll() throws DataException;
+
+	public T doSave(T entity) throws DataException;
+
+	public T doUpdate(T entity) throws DataException;
+
+	public void doDelete(T entity) throws DataException;
+}
