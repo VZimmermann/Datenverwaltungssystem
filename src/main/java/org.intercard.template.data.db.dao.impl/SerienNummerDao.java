d57bceed390ee435d11cc8eb0498466a20439087
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-15 12:11:41 +0200

    Update SerienNummerDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/SerienNummerDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/SerienNummerDao.java
@@ -1,26 +1,50 @@
 package org.intercard.template.data.db.dao.impl;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Query;
 
-import org.intercard.template.data.db.dao.ISerienNummernDao;
-import org.intercard.template.data.db.domain.SerienNummern;
+import org.intercard.template.data.db.dao.ISerienNummerDao;
+import org.intercard.template.data.db.domain.SerienNummer;
 import org.intercard.template.data.db.ex.DataException;
 
-public class SerienNummernDao extends AbstractDao<SerienNummern> implements
-		ISerienNummernDao {
+public class SerienNummerDao extends AbstractDao<SerienNummer> implements
+		ISerienNummerDao {
 
 	@Override
-	protected Class<SerienNummern> getDomainClass() {
-		return SerienNummern.class;
+	protected Class<SerienNummer> getDomainClass() {
+		return SerienNummer.class;
 	}
 
+	/** Find all Seriennummern */
+	@SuppressWarnings("unchecked")
 	@Override
-	public SerienNummern findSerienNummern(String tName) throws DataException {
-
-		Query q = entityManager.createNamedQuery("findSerienNummern");
-		q.setParameter("name", tName);
-
-		// GENAU einer erwartet.
-		return (SerienNummern) q.getSingleResult();
+	public List<SerienNummer> findAllSeriennummern() throws DataException {
+		Query q = entityManager.createNamedQuery("findAllSeriennummern");
+		return (List<SerienNummer>) q.getResultList();
 	}
+
+	/**
+	 * Find Seriennummernbereiche (Ranges)-->Wenn man die erste Nummer hat wird
+	 * die Anzahl angezeigt
+	 */
+	@Override
+	public SerienNummer findSeriennummeranfang(Long snanfang)
+			throws DataException {
+		Query q = entityManager.createNamedQuery("findSeriennummerbySNAnfang");
+		q.setParameter("snanfang", snanfang);
+
+		return (SerienNummer) q.getSingleResult();
+	}
+
+	/** Find Seriennummern with a special date when they were created */
+	@Override
+	public SerienNummer findSeriennummerbydate(Date date) throws DataException {
+		Query q = entityManager.createNamedQuery("findSeriennummerbyDate");
+		q.setParameter("date", date);
+
+		return (SerienNummer) q.getSingleResult();
+	}
+
 }
