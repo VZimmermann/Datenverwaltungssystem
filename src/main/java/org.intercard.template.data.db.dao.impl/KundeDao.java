0d325fb0dfb7f2201aa105655d63ae8425226cd8
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-07-15 08:25:51 +0200

    Create KundeDao.java

---
+++ src/main/java/org.intercard.template.data.db.dao.impl/KundeDao.java
@@ -0,0 +1,48 @@
+package org.intercard.template.data.db.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.intercard.template.data.db.dao.IKundeDao;
+import org.intercard.template.data.db.domain.Kunde;
+import org.intercard.template.data.db.ex.DataException;
+
+public class KundeDao extends AbstractDao<Kunde> implements IKundeDao {
+
+	@Override
+	protected Class<Kunde> getDomainClass() {
+		return Kunde.class;
+	}
+
+	@Override
+	public Kunde findbyName(String tName) throws DataException {
+
+		Query q = entityManager.createNamedQuery("findKundebyName");
+		q.setParameter("name", tName);
+
+		// GENAU einer erwartet.
+		return (Kunde) q.getSingleResult();
+	}
+
+	@Override
+	public Kunde findWithBags(Long id) throws DataException {
+
+		Query q = entityManager.createNamedQuery("findKundeWithBags");
+		q.setParameter("id", id);
+
+		// GENAU einer erwartet.
+		return (Kunde) q.getSingleResult();
+	}
+
+	/** if there is a warning don#t show it */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Kunde> findAllWithBags() throws DataException {
+
+		Query q = entityManager.createNamedQuery("findAllKundeWithBags");
+
+		// GENAU einer erwartet.
+		return (List<Kunde>) q.getResultList();
+	}
+}
