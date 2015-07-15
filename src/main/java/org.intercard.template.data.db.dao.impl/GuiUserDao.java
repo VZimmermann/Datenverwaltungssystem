ca7a95402456fa9373b944afe8d1abdb6cea1ddb
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-15 12:10:41 +0200

    Update GuiUserDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/GuiUserDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/GuiUserDao.java
@@ -1,5 +1,7 @@
 package org.intercard.template.data.db.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.Query;
 
 import org.intercard.template.data.db.dao.IGuiUserDao;
@@ -13,6 +15,7 @@ public class GuiUserDao extends AbstractDao<GuiUser> implements IGuiUserDao {
 		return GuiUser.class;
 	}
 
+	/** Find GuiUser by Name */
 	@Override
 	public GuiUser findByName(String tName) throws DataException {
 
@@ -21,5 +24,22 @@ public class GuiUserDao extends AbstractDao<GuiUser> implements IGuiUserDao {
 
 		// GENAU einer erwartet.
 		return (GuiUser) q.getSingleResult();
+	}
+
+	/** Find GuiUser by ID */
+	@Override
+	public GuiUser findByID(Long id) throws DataException {
+		Query q = entityManager.createNamedQuery("findGUserByID");
+		q.setParameter("id", id);
+
+		return (GuiUser) q.getSingleResult();
+	}
+
+	/** Find all existing GuiUser */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<GuiUser> findAllGuiUser() throws DataException {
+		Query q = entityManager.createNamedQuery("findAllGUser");
+		return (List<GuiUser>) q.getResultList();
 	}
 }
