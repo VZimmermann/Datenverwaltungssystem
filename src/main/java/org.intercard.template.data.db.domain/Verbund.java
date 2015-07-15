e27e734163677e0e2221447a1e913af99eb825fa
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-07-15 08:36:36 +0200

    Create Verbund.java

---
+++ src/main/java/org.intercard.template.data.db.domain/Verbund.java
@@ -0,0 +1,92 @@
+package org.intercard.template.data.db.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.intercard.template.data.db.IEntity;
+
+/**
+ * 
+ * @author vanessa zimmermann
+ *
+ */
+@Entity
+@Table(name = "Verbund")
+@NamedQueries({ @NamedQuery(name = "findVerbund", query = "SELECT v FROM Verbund v where v.verbundname = :name") })
+public class Verbund implements IEntity {
+
+	/**
+	 * 
+	 */
+	@Transient
+	private static final long serialVersionUID = -4976548298747446L;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "Verbundname", unique = true)
+	private String verbundname;
+
+	// @ManyToOne
+	// private Kunde verbundmutter;
+
+	@OneToMany(mappedBy = "verbund")
+	private List<Kunde> verbundsTeilnehmer;
+
+	// ++++++++++++++++++++++++++++++++++++
+	public Verbund() {
+
+	}
+
+	// ++++++++++++++++++++++++++++++++++++
+	// Getter and Setter
+	// ++++++++++++++++++++++++++++++++++++
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	public String getVerbundname() {
+		return verbundname;
+	}
+
+	public void setVerbundname(String verbundname) {
+		this.verbundname = verbundname;
+	}
+
+	public List<Kunde> getVerbundsTeilnehmer() {
+		if (verbundsTeilnehmer == null)
+			return new ArrayList<Kunde>();
+		return verbundsTeilnehmer;
+	}
+
+	public void setVerbundsTeilnehmer(List<Kunde> verbundsTeilnehmer) {
+		this.verbundsTeilnehmer = verbundsTeilnehmer;
+	}
+
+	public void addVerbundsTeilnehmer(Kunde verbund) {
+		if (verbundsTeilnehmer == null) {
+			verbundsTeilnehmer = new ArrayList<Kunde>();
+		}
+		verbundsTeilnehmer.add(verbund);
+	}
+
+	@Override
+	public String toString() {
+		return "Verbund [ID=" + id + ", Verbundname=" + verbundname + "]";
+	}
+
+}
