9623ec205c940259e74ef780efc298985ea3129c
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-15 12:09:44 +0200

    Update ClassicDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/ClassicDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/ClassicDao.java
@@ -1,5 +1,7 @@
 package org.intercard.template.data.db.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.Query;
 
 import org.intercard.template.data.db.dao.IClassicDao;
@@ -13,13 +15,33 @@ public class ClassicDao extends AbstractDao<Classic> implements IClassicDao {
 		return Classic.class;
 	}
 
+	/** Find all active Classic cardtechnologies */
 	@Override
-	public Classic findClassic(String tName) throws DataException {
+	public Classic findActivClassic(boolean activ) throws DataException {
 
-		Query q = entityManager.createNamedQuery("findClassic");
-		q.setParameter("systemnummer", tName);
+		Query q = entityManager.createNamedQuery("findActivClassic");
+		q.setParameter("activ", activ);
 
 		// GENAU einer erwartet.
 		return (Classic) q.getSingleResult();
+	}
+
+	/** Find all Classic cards */
+	@Override
+	public Classic findClassicbyID(String id) throws DataException {
+		Query q = entityManager.createNamedQuery("findClassicbyID");
+		q.setParameter("systemnummergesamt", id);
+
+		// GENAU einer erwartet
+		return (Classic) q.getSingleResult();
+	}
+
+	/** Find classic by N1V/N1A/N2/N3 connected to String */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Classic> findAllClassic() throws DataException {
+		Query q = entityManager.createNamedQuery("findAllClassic");
+
+		return (List<Classic>) q.getResultList();
 	}
 }
