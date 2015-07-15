114db92a6133b4f15b457d52ebceba087faa5fa9
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-15 12:06:22 +0200

    Update IClassicDao.java

--- src/main/java/org.intercard.template.data.db.dao/IClassicDao.java
+++ src/main/java/org.intercard.template.data.db.dao/IClassicDao.java
@@ -1,10 +1,18 @@
 package org.intercard.template.data.db.dao;
 
+import java.util.List;
+
 import org.intercard.template.data.db.domain.Classic;
 import org.intercard.template.data.db.ex.DataException;
 
 public interface IClassicDao extends IDao<Classic> {
 
-	public Classic findClassic(String tName) throws DataException;
+	/** Find all active Classic cardtechnologies */
+	public Classic findActivClassic(boolean activ) throws DataException;
 
+	/** Find classic by N1V/N1A/N2/N3 connected to String */
+	public Classic findClassicbyID(String id) throws DataException;
+
+	/** Find all Classic cards */
+	public List<Classic> findAllClassic() throws DataException;
 }
