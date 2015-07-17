760e4407265310d98908c4c9b2292bd977c167ff
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-17 10:27:55 +0200

    Create CDKAuftragsdeckblattGenerierungService.java

---
+++ src/main/java/org.intercard.template.service.impl/CDKAuftragsdeckblattGenerierungService.java
@@ -0,0 +1,13 @@
+package org.intercard.template.service.impl;
+
+import org.intercard.template.service.ICDKAuftragsdeckblattGenerierungService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CDKAuftragsdeckblattGenerierungService implements
+		ICDKAuftragsdeckblattGenerierungService {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(CDKAuftragsdeckblattGenerierungService.class);
+
+}
