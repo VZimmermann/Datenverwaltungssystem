d88c4ba044192cd2f62f348a26fba8860f25db68
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-17 10:16:21 +0200

    Update ILegicDao.java

--- src/main/java/org.intercard.template.data.db.dao/ILegicDao.java
+++ src/main/java/org.intercard.template.data.db.dao/ILegicDao.java
@@ -8,10 +8,10 @@ import org.intercard.template.data.db.ex.DataException;
 public interface ILegicDao extends IDao<Legic> {
 
 	/** Find all active Legic cardtechnologies */
-	public Legic findActivLegic(boolean activ) throws DataException;
+	public Legic findActivLegic(boolean aktiv) throws DataException;
 
-	/** Find Legic by N1V/N1A/N2/N3 connected to String */
-	public Legic findLegicbyID(String id) throws DataException;
+	// /** Find Legic by N1V/N1A/N2/N3 connected to String */
+	// public Legic findLegicbyID(String id) throws DataException;
 
 	/** Find all Legic cards */
 	public List<Legic> findAllLegic() throws DataException;
