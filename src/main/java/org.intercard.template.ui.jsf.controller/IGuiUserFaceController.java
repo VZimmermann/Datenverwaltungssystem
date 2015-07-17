242a317860db684719c4cc406f6009c71c21a80d
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-17 10:35:22 +0200

    Create IGuiUserFaceController.java

---
+++ src/main/java/org.intercard.template.ui.jsf.controller/IGuiUserFaceController.java
@@ -0,0 +1,13 @@
+package org.intercard.template.ui.jsf.controller;
+
+public interface IGuiUserFaceController {
+
+	public String load();
+
+	public String edit();
+
+	public String delete();
+
+	public boolean isListNotEmpty();
+
+}
