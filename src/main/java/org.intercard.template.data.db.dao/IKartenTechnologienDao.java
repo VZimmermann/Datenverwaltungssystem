81fef92bebaa3a77b6705e03bc277caeb991efe9
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-17 10:15:50 +0200

    Update IKartenTechnologienDao.java

--- src/main/java/org.intercard.template.data.db.dao/IKartenTechnologienDao.java
+++ src/main/java/org.intercard.template.data.db.dao/IKartenTechnologienDao.java
@@ -2,18 +2,13 @@ package org.intercard.template.data.db.dao;
 
 import java.util.List;
 
-import org.intercard.template.data.db.domain.GuiUser;
+import org.intercard.template.data.db.domain.KartenTechnologien;
 import org.intercard.template.data.db.ex.DataException;
 
-public interface IGuiUserDao extends IDao<GuiUser> {
+public interface IKartenTechnologienDao extends IDao<KartenTechnologien> {
 
-	/** Find GuiUser by Name */
-	public GuiUser findByName(String tName) throws DataException;
-
-	/** Find GuiUser by ID */
-	public GuiUser findByID(Long id) throws DataException;
-
-	/** Find all existing GuiUser */
-	public List<GuiUser> findAllGuiUser() throws DataException;
+	/** find all existing CardTechnologies with type KartenTechnologie */
+	public List<KartenTechnologien> findKartentechnologien()
+			throws DataException;
 
 }
