b48c084c88393ea93e7210d878cf9c2f136e066c
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-17 10:21:15 +0200

    Update Verbundschluessel.java

--- src/main/java/org.intercard.template.data.db.domain/Verbundschluessel.java
+++ src/main/java/org.intercard.template.data.db.domain/Verbundschluessel.java
@@ -2,17 +2,15 @@ package org.intercard.template.data.db.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import org.intercard.template.data.db.IEntity;
-import org.intercard.template.data.db.domain.enums.VerbundschluesselEnum;
 
 /**
  * 
@@ -22,6 +20,9 @@ import org.intercard.template.data.db.domain.enums.VerbundschluesselEnum;
 
 @Entity
 @Table(name = "Verbundschluessel")
+// @NamedQueries({ @NamedQuery(name = "findSchluesseltoKunde", query =
+// "SELECT s FROM Verbundschluessel s join fetch s.kunde k where kunde.id= :id ")
+// })
 public class Verbundschluessel implements IEntity {
 
 	/**
@@ -33,26 +34,99 @@ public class Verbundschluessel implements IEntity {
 	@Id
 	@Column(name = "ID")
 	@GeneratedValue(strategy = GenerationType.TABLE)
-	private Long id;
+	private int id;
 
-	@Column(name = "schluessel")
-	@Enumerated(EnumType.STRING)
-	private VerbundschluesselEnum schluessel;
+	// @Column(name = "schluessel")
+	// @Enumerated(EnumType.STRING)
+	// private VerbundschluesselEnum schluessel;
+
+	@Column(name = "Hauptboerse")
+	private boolean hauptboerse;
+
+	@Column(name = "OeffenDaten")
+	private boolean oeffenDaten;
+
+	@Column(name = "MTKNR_TRW_DATEN")
+	private boolean mtknr_trw_daten;
+
+	@Column(name = "Bibliotheksnr")
+	private boolean bibliotheksnr;
+
+	@Column(name = "Zutrittsnr")
+	private boolean zutrittsnr;
 
 	@ManyToOne
+	@JoinColumn
 	private Kunde kunde;
 
 	// +++Getter Setter+++//
 
-	public Long getId() {
+	@Override
+	public String toString() {
+		return "Verbundschluessel [" + "hauptboerse=" + hauptboerse
+				+ ", oeffenDaten=" + oeffenDaten + ", mtknr_trw_daten="
+				+ mtknr_trw_daten + ", bibliotheksnr=" + bibliotheksnr
+				+ ", zutrittsnr=" + zutrittsnr + "]";
+	}
+
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	public int getId() {
 		return id;
 	}
 
-	public VerbundschluesselEnum getSchluessel() {
-		return schluessel;
+	public boolean isHauptboerse() {
+		return hauptboerse;
 	}
 
-	public void setSchluessel(VerbundschluesselEnum schluessel) {
-		this.schluessel = schluessel;
+	public void setHauptboerse(boolean hauptboerse) {
+		this.hauptboerse = hauptboerse;
 	}
+
+	public boolean isOeffenDaten() {
+		return oeffenDaten;
+	}
+
+	public void setOeffenDaten(boolean oeffenDaten) {
+		this.oeffenDaten = oeffenDaten;
+	}
+
+	public boolean isMtknr_trw_daten() {
+		return mtknr_trw_daten;
+	}
+
+	public void setMtknr_trw_daten(boolean mtknr_trw_daten) {
+		this.mtknr_trw_daten = mtknr_trw_daten;
+	}
+
+	public boolean isBibliotheksnr() {
+		return bibliotheksnr;
+	}
+
+	public void setBibliotheksnr(boolean bibliotheksnr) {
+		this.bibliotheksnr = bibliotheksnr;
+	}
+
+	public boolean isZutrittsnr() {
+		return zutrittsnr;
+	}
+
+	public void setZutrittsnr(boolean zutrittsnr) {
+		this.zutrittsnr = zutrittsnr;
+	}
+
+	// public VerbundschluesselEnum getSchluessel() {
+	// return schluessel;
+	// }
+	//
+	// public void setSchluessel() {
+	// this.schluessel = schluessel;
+	// }
+
 }
