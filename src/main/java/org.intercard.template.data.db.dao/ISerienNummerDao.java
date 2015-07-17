c18c11064fa4bbe4b6ef007eb8895811ec7792fb
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-17 10:16:32 +0200

    Update ISerienNummerDao.java

--- src/main/java/org.intercard.template.data.db.dao/ISerienNummerDao.java
+++ src/main/java/org.intercard.template.data.db.dao/ISerienNummerDao.java
@@ -1,6 +1,5 @@
 package org.intercard.template.data.db.dao;
 
-import java.util.Date;
 import java.util.List;
 
 import org.intercard.template.data.db.domain.SerienNummer;
@@ -15,10 +14,8 @@ public interface ISerienNummerDao extends IDao<SerienNummer> {
 	 * Find Seriennummernbereiche (Ranges)-->Wenn man die erste Nummer hat wird
 	 * die Anzahl angezeigt
 	 */
-	public SerienNummer findSeriennummeranfang(Long snanfang)
-			throws DataException;
 
-	/** Find Seriennummern with a special date when they were created */
-	public SerienNummer findSeriennummerbydate(Date date) throws DataException;
+	public SerienNummer findSeriennummeranfang(int snanfang)
+			throws DataException;
 
 }
