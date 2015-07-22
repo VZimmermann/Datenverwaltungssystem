995022bd2ca11bd2c75bde5a9183bee1a74e6ae4
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:37:25 +0200

    Update SerienNummerDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/SerienNummerDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/SerienNummerDao.java
@@ -1,50 +1,166 @@
-package org.intercard.template.data.db.dao.impl;
+package org.intercard.template.data.db.domain;
 
-import java.util.Date;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-import javax.persistence.Query;
+import org.intercard.template.data.db.IEntity;
 
-import org.intercard.template.data.db.dao.ISerienNummerDao;
-import org.intercard.template.data.db.domain.SerienNummer;
-import org.intercard.template.data.db.ex.DataException;
+/**
+ * GUIUSER for entity.
+ * 
+ * @author Vanessa Zimmermann
+ * 
+ */
+@Entity
+@Table(name = "SerienNummer")
+@NamedQueries({
+		@NamedQuery(name = "findAllSeriennummern", query = "SELECT s FROM SerienNummer s"),
+		@NamedQuery(name = "findSeriennummerbySNAnfang", query = "SELECT s FROM SerienNummer s where s.snanfang = :snanfang") })
+public class SerienNummer implements IEntity {
 
-public class SerienNummerDao extends AbstractDao<SerienNummer> implements
-		ISerienNummerDao {
+	@Transient
+	private static final long serialVersionUID = -9765482747446L;
 
-	@Override
-	protected Class<SerienNummer> getDomainClass() {
-		return SerienNummer.class;
+	@Id
+	@Column(name = "SNAnfang")
+	private int snanfang;
+
+	@Column(name = "SNEnde", unique = true)
+	private int snende;
+
+	@Column(name = "Kommentar", nullable = true)
+	private String kommentar;
+
+	@Column(name = "vergeben_am")
+	private String vergebenam;
+
+	@Column(name = "Tag")
+	private int tag;
+
+	@Column(name = "Monat")
+	private int monat;
+
+	@Column(name = "Jahr")
+	private int jahr;
+
+	@Column(name = "Anzahl", nullable = true)
+	private int anzahl;
+
+	@ManyToOne
+	@JoinColumn
+	private Kunde kunde;
+
+	// +++++++++++++++++++++++++++++++++++++
+	public SerienNummer() {
+
 	}
 
-	/** Find all Seriennummern */
-	@SuppressWarnings("unchecked")
+	// ++++++++++++++++++++++++++++++++++++
+	// Getter and Setter
+	// ++++++++++++++++++++++++++++++++++++
+
 	@Override
-	public List<SerienNummer> findAllSeriennummern() throws DataException {
-		Query q = entityManager.createNamedQuery("findAllSeriennummern");
-		return (List<SerienNummer>) q.getResultList();
+	public int getId() {
+		return snanfang;
 	}
 
-	/**
-	 * Find Seriennummernbereiche (Ranges)-->Wenn man die erste Nummer hat wird
-	 * die Anzahl angezeigt
-	 */
-	@Override
-	public SerienNummer findSeriennummeranfang(Long snanfang)
-			throws DataException {
-		Query q = entityManager.createNamedQuery("findSeriennummerbySNAnfang");
-		q.setParameter("snanfang", snanfang);
-
-		return (SerienNummer) q.getSingleResult();
+	public int getSnanfang() {
+		return snanfang;
 	}
 
-	/** Find Seriennummern with a special date when they were created */
-	@Override
-	public SerienNummer findSeriennummerbydate(Date date) throws DataException {
-		Query q = entityManager.createNamedQuery("findSeriennummerbyDate");
-		q.setParameter("date", date);
+	public int getSnende() {
+		return snende;
+	}
 
-		return (SerienNummer) q.getSingleResult();
+	public void setSnende(int snende) {
+		this.snende = snende;
+	}
+
+	public String getKommentar() {
+
+		return kommentar;
+
+	}
+
+	public void setKommentar(String kommentar) {
+		if (kommentar != null)
+			this.kommentar = kommentar;
+
+	}
+
+	public void setSnanfang(int snanfang) {
+		this.snanfang = snanfang;
+	}
+
+	public String getVergebenam() {
+		return vergebenam;
+	}
+
+	public void setVergebenam() {
+		vergebenam = tag + "." + monat + "." + jahr;
+	}
+
+	public int getAnzahl() {
+		return anzahl;
+	}
+
+	public void setAnzahl() {
+
+		anzahl = snende - snanfang;
+
+	}
+
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	public int getTag() {
+		return tag;
+	}
+
+	public void setTag(int tag) {
+		if (tag <= 31) {
+			this.tag = tag;
+		}
+	}
+
+	public int getMonat() {
+		return monat;
+	}
+
+	public void setMonat(int monat) {
+		if (monat <= 12) {
+			this.monat = monat;
+		}
+	}
+
+	public int getJahr() {
+		return jahr;
+	}
+
+	public void setJahr(int jahr) {
+		this.jahr = jahr;
+	}
+
+	@Override
+	public String toString() {
+		return "SerienNummer [snanfang=" + snanfang + ", snende=" + snende
+				+ ", "
+				+ (kommentar != null ? "kommentar=" + kommentar + ", " : "")
+				+ (vergebenam != null ? "vergebenam=" + vergebenam + ", " : "")
+				+ "tag=" + tag + ", monat=" + monat + ", jahr=" + jahr
+				+ ", anzahl=" + anzahl + "]";
 	}
 
 }
