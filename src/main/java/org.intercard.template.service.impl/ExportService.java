1ba9d1db49e7e1b701918833b985e0f7ae88e729
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:43:34 +0200

    Update ExportService.java

--- src/main/java/org.intercard.template.service.impl/ExportService.java
+++ src/main/java/org.intercard.template.service.impl/ExportService.java
@@ -1,9 +1,6 @@
 package org.intercard.template.service.impl;
 
-import org.intercard.template.data.db.domain.Classic;
-import org.intercard.template.data.db.domain.Desfire;
 import org.intercard.template.data.db.domain.Kunde;
-import org.intercard.template.data.db.domain.Legic;
 import org.intercard.template.service.IExportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,24 +14,6 @@ public class ExportService implements IExportService {
 	public String createExportpfad() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public int getSystemnummer(Classic systemnummer) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getSystemnummer(Legic systemnummer) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getSystemnummer(Desfire systemnummer) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
