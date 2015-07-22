ee7c24a3f2842f38e05ea5c46b0a6dd4c87fdea5
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:36:26 +0200

    Update LegicDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/LegicDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/LegicDao.java
@@ -1,40 +1,104 @@
-package org.intercard.template.data.db.dao.impl;
+package org.intercard.template.data.db.domain;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-import javax.persistence.Query;
+import org.intercard.template.data.db.IEntity;
 
-import org.intercard.template.data.db.dao.ISerienNummerDao;
-import org.intercard.template.data.db.domain.SerienNummer;
-import org.intercard.template.data.db.ex.DataException;
-
-public class SerienNummerDao extends AbstractDao<SerienNummer> implements
-		ISerienNummerDao {
-
-	@Override
-	protected Class<SerienNummer> getDomainClass() {
-		return SerienNummer.class;
-	}
-
-	/** Find all Seriennummern */
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<SerienNummer> findAllSeriennummern() throws DataException {
-		Query q = entityManager.createNamedQuery("findAllSeriennummern");
-		return (List<SerienNummer>) q.getResultList();
-	}
+/**
+ * 
+ * @author vanessa zimmermann
+ *
+ */
+@Entity
+@Table(name = "Legic")
+@NamedQueries({
+		@NamedQuery(name = "findAllLegic", query = "SELECT l FROM Legic l"),
+		@NamedQuery(name = "findActivLegic", query = "SELECT l FROM Legic l where l.aktiv = :activ") })
+public class Legic extends KartenTechnologien implements IEntity {
 
 	/**
-	 * Find Seriennummernbereiche (Ranges)-->Wenn man die erste Nummer hat wird
-	 * die Anzahl angezeigt
+	 * 
 	 */
-	@Override
-	public SerienNummer findSeriennummeranfang(int snanfang)
-			throws DataException {
-		Query q = entityManager.createNamedQuery("findSeriennummerbySNAnfang");
-		q.setParameter("snanfang", snanfang);
+	@Transient
+	private static final long serialVersionUID = -15176857446L;
 
-		return (SerienNummer) q.getSingleResult();
+	@Column(name = "N1V", nullable = true)
+	private Integer N1V;
+
+	@Column(name = "N1A", nullable = true)
+	private Integer N1A;
+
+	@Column(name = "N2", nullable = true)
+	private Integer N2;
+
+	@Column(name = "N3", nullable = true)
+	private Integer N3;
+
+	@Column(name = "Kartentechnologie")
+	private String kartentechnologie = "Legic";
+
+	@Column(name = "zusammengesetzt")
+	private String zusammengesetzt;
+
+	// ++++++++++++++++++++++++++++++++++++
+	public Legic() {
+
 	}
 
+	// ++++++++++++++++++++++++++++++++++++
+	// Getter and Setter
+	// ++++++++++++++++++++++++++++++++++++
+
+	@Override
+	public String toString() {
+		return "Legic[" + super.toString() + ", Systemnummer=" + N1V + "/"
+				+ N1A + "." + N2 + "." + N3 + "]";
+	}
+
+	public Integer getN1V() {
+		return N1V;
+	}
+
+	public void setN1V(Integer n1v) {
+		N1V = n1v;
+	}
+
+	public Integer getN1A() {
+		return N1A;
+	}
+
+	public void setN1A(Integer n1a) {
+		N1A = n1a;
+	}
+
+	public Integer getN2() {
+		return N2;
+	}
+
+	public void setN2(Integer n2) {
+		N2 = n2;
+	}
+
+	public Integer getN3() {
+		return N3;
+	}
+
+	public void setN3(Integer n3) {
+		N3 = n3;
+	}
+
+	public String getZusammengesetzt() {
+		return zusammengesetzt;
+	}
+
+	public void setZusammengesetzt() {
+
+		zusammengesetzt = N1V + "/" + N1A + "." + N2 + "." + N3;
+
+	}
 }
