81a1c74cb1f9eea3c23bd22a7a4b32bad302bd3b
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:37:40 +0200

    Update VerbundDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/VerbundDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/VerbundDao.java
@@ -1,48 +1,128 @@
-package org.intercard.template.data.db.dao.impl;
+package org.intercard.template.data.db.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Query;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-import org.intercard.template.data.db.dao.IVerbundDao;
-import org.intercard.template.data.db.domain.Verbund;
-import org.intercard.template.data.db.ex.DataException;
+import org.intercard.template.data.db.IEntity;
 
-public class VerbundDao extends AbstractDao<Verbund> implements IVerbundDao {
+/**
+ * 
+ * @author vanessa zimmermann
+ *
+ */
+@Entity
+@Table(name = "Verbund")
+@NamedQueries({
+		@NamedQuery(name = "findVerbund", query = "SELECT v FROM Verbund v where v.verbundname = :name"),
+		@NamedQuery(name = "findAllVerbund", query = "SELECT v FROM Verbund v "),
 
-	@Override
-	protected Class<Verbund> getDomainClass() {
-		return Verbund.class;
+/*
+ * @NamedQuery(name = "findVerbundWithTeilnehmer", query =
+ * " Select v, k FROM Verbund v ,Kunde k join fetch v.verbundsTeilnehmer k ")
+ */})
+public class Verbund implements IEntity {
+
+	/**
+	 * 
+	 */
+	@Transient
+	private static final long serialVersionUID = -4976548298747446L;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private int id;
+
+	@Column(name = "Verbundname")
+	private String verbundname;
+
+	// @ManyToOne
+	// private Kunde verbundmutter;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "verbund", fetch = FetchType.EAGER)
+	private List<Kunde> verbundsTeilnehmer;
+
+	// ++++++++++++++++++++++++++++++++++++
+	public Verbund() {
+
 	}
 
-	/** find a special Verbund by name */
+	// ++++++++++++++++++++++++++++++++++++
+	// Getter and Setter
+	// ++++++++++++++++++++++++++++++++++++
+
 	@Override
-	public Verbund findVerbund(String name) throws DataException {
-
-		Query q = entityManager.createNamedQuery("findVerbund");
-		q.setParameter("name", name);
-
-		// GENAU einer erwartet.
-		return (Verbund) q.getSingleResult();
+	public int getId() {
+		return id;
 	}
 
-	/** Find all existing Verbünde */
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Verbund> findAllVerbund() throws DataException {
-		Query q = entityManager.createNamedQuery("findAllVerbund");
-		return (List<Verbund>) q.getResultList();
+	public String getVerbundname() {
+		return verbundname;
 	}
 
-	/** Find a Verbund with its Teilnehmer */
+	public void setVerbundname(String verbundname) {
+		this.verbundname = verbundname;
+	}
+
+	public List<Kunde> getVerbundsTeilnehmer() {
+		if (verbundsTeilnehmer == null)
+			return new ArrayList<Kunde>();
+		return verbundsTeilnehmer;
+	}
+
+	public void setVerbundsTeilnehmer(List<Kunde> verbundsTeilnehmer) {
+		this.verbundsTeilnehmer = verbundsTeilnehmer;
+	}
+
+	public void addVerbundsTeilnehmer(Kunde verbund) {
+		if (verbundsTeilnehmer == null) {
+			verbundsTeilnehmer = new ArrayList<Kunde>();
+		}
+		verbundsTeilnehmer.add(verbund);
+	}
+
 	@Override
-	public Verbund findVerbundWithTeilnehmer(String tName) throws DataException {
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Verbund [id=");
+		builder.append(id);
+		builder.append(", ");
+		if (verbundname != null) {
+			builder.append("verbundname=");
+			builder.append(verbundname);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
 
-		Query q = entityManager.createNamedQuery("findVerbundWithTeilnehmer");
-		q.setParameter("name", tName);
-
-		// GENAU einer erwartet.
-		return (Verbund) q.getSingleResult();
+	public String toStringWithVerbundteilnehmer() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Verbund [id=");
+		builder.append(id);
+		builder.append(", ");
+		if (verbundname != null) {
+			builder.append("verbundname=");
+			builder.append(verbundname);
+			builder.append(", ");
+		}
+		if (verbundsTeilnehmer != null) {
+			builder.append("verbundsTeilnehmer=");
+			builder.append(verbundsTeilnehmer);
+		}
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
