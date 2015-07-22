bd747d42fcbf9deb960b5119dd8471c4e673d62e
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:37:57 +0200

    Update VerbundschluesselDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/VerbundschluesselDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/VerbundschluesselDao.java
@@ -1,24 +1,148 @@
-package org.intercard.template.data.db.dao.impl;
+package org.intercard.template.data.db.domain;
 
-import org.intercard.template.data.db.dao.IVerbundschluesselDao;
-import org.intercard.template.data.db.domain.Verbundschluessel;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-public class VerbundschluesselDao extends AbstractDao<Verbundschluessel>
-		implements IVerbundschluesselDao {
+import org.intercard.template.data.db.IEntity;
+
+/**
+ * 
+ * @author vanessa zimmermann
+ *
+ */
+
+@Entity
+@Table(name = "Verbundschluessel")
+@NamedQueries({ @NamedQuery(name = "findSchluessel", query = "SELECT s FROM Verbundschluessel s") })
+public class Verbundschluessel implements IEntity {
+
+	/**
+	 * 
+	 */
+	@Transient
+	private static final long serialVersionUID = -249971297446L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private int id;
+
+	// @Column(name = "schluessel")
+	// @Enumerated(EnumType.STRING)
+	// private VerbundschluesselEnum schluessel;
+
+	@Column(name = "Hauptboerse")
+	private boolean hauptboerse;
+
+	@Column(name = "OeffenDaten")
+	private boolean oeffenDaten;
+
+	@Column(name = "MTKNR_TRW_DATEN")
+	private boolean mtknr_trw_daten;
+
+	@Column(name = "Bibliotheksnr")
+	private boolean bibliotheksnr;
+
+	@Column(name = "Zutrittsnr")
+	private boolean zutrittsnr;
+
+	@ManyToOne
+	@JoinColumn
+	private Kunde kunde;
+
+	// +++Getter Setter+++//
+
+	// @Override
+	// public String toString() {
+	// return "Verbundschluessel[id="
+	// + id
+	// + ","
+	// + (hauptboerse == true ? "hauptboerse=" + true + ", " : "")
+	// + (oeffenDaten == true ? "oeffenDaten=" + true + ", " : "")
+	// + (mtknr_trw_daten == true ? "mtknr_trw_daten=" + true + ", "
+	// : "")
+	// + (bibliotheksnr == true ? "bibliotheksnr=" + true + ", " : "")
+	// + (zutrittsnr == true ? "zutrittsnr=" + true : "") + "]";
+	// }
 
 	@Override
-	protected Class<Verbundschluessel> getDomainClass() {
-		return Verbundschluessel.class;
+	public String toString() {
+		/** gibt nur das aus, was true ist */
+		return "Verbundschluessel[id=" + id + ","
+				+ (hauptboerse == true ? "hauptboerse" + ", " : "")
+				+ (oeffenDaten == true ? "oeffenDaten" + "," : "")
+				+ (mtknr_trw_daten == true ? "mtknr_trw_daten" + ", " : "")
+				+ (bibliotheksnr == true ? "bibliotheksnr" + ", " : "")
+				+ (zutrittsnr == true ? "zutrittsnr" : "") + "]";
 	}
 
-	// @SuppressWarnings("unchecked")
-	// @Override
-	// public List<Verbundschluessel> findSchluesseltoKunde() throws
-	// DataException {
-	// Query q = entityManager.createNamedQuery("findSchluesseltoKunde");
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public boolean isHauptboerse() {
+		return hauptboerse;
+	}
+
+	public void setHauptboerse(boolean hauptboerse) {
+		this.hauptboerse = hauptboerse;
+	}
+
+	public boolean isOeffenDaten() {
+		return oeffenDaten;
+	}
+
+	public void setOeffenDaten(boolean oeffenDaten) {
+		this.oeffenDaten = oeffenDaten;
+	}
+
+	public boolean isMtknr_trw_daten() {
+		return mtknr_trw_daten;
+	}
+
+	public void setMtknr_trw_daten(boolean mtknr_trw_daten) {
+		this.mtknr_trw_daten = mtknr_trw_daten;
+	}
+
+	public boolean isBibliotheksnr() {
+		return bibliotheksnr;
+	}
+
+	public void setBibliotheksnr(boolean bibliotheksnr) {
+		this.bibliotheksnr = bibliotheksnr;
+	}
+
+	public boolean isZutrittsnr() {
+		return zutrittsnr;
+	}
+
+	public void setZutrittsnr(boolean zutrittsnr) {
+		this.zutrittsnr = zutrittsnr;
+	}
+
+	// public VerbundschluesselEnum getSchluessel() {
+	// return schluessel;
+	// }
 	//
-	// // GENAU einer erwartet.
-	// return (List<Verbundschluessel>) q.getResultList();
+	// public void setSchluessel() {
+	// this.schluessel = schluessel;
 	// }
 
 }
