7097eb15b3ff466cf43e6d5be9f13c6796d8c1ad
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:33:57 +0200

    Update IVerbundschluesselDao.java

--- src/main/java/org.intercard.template.data.db.dao/IVerbundschluesselDao.java
+++ src/main/java/org.intercard.template.data.db.dao/IVerbundschluesselDao.java
@@ -4,7 +4,6 @@ import org.intercard.template.data.db.domain.Verbundschluessel;
 
 public interface IVerbundschluesselDao extends IDao<Verbundschluessel> {
 
-	// public List<Verbundschluessel> findSchluesseltoKunde() throws
-	// DataException;
+	public List<Verbundschluessel> findSchluessel() throws DataException;
 
 }
