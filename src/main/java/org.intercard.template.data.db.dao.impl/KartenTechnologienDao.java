1775b7a57c5ea3011cd4b422eb60ffe618509b66
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:35:55 +0200

    Update KartenTechnologienDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/KartenTechnologienDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/KartenTechnologienDao.java
@@ -1,28 +1,91 @@
-package org.intercard.template.data.db.dao.impl;
+package org.intercard.template.data.db.domain;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Transient;
 
-import javax.persistence.Query;
+import org.intercard.template.data.db.IEntity;
 
-import org.intercard.template.data.db.dao.IKartenTechnologienDao;
-import org.intercard.template.data.db.domain.KartenTechnologien;
-import org.intercard.template.data.db.ex.DataException;
+/**
+ * 
+ * @author vanessa zimmermann
+ *
+ */
 
-public class KartenTechnologienDao extends AbstractDao<KartenTechnologien>
-		implements IKartenTechnologienDao {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity
+@NamedQueries({ @NamedQuery(name = "findKartentechnologien", query = "SELECT k from KartenTechnologien k") })
+public abstract class KartenTechnologien implements IEntity {
 
-	@Override
-	protected Class<KartenTechnologien> getDomainClass() {
-		return KartenTechnologien.class;
+	/**
+	 * 
+	 */
+	@Transient
+	private static final long serialVersionUID = -24976971297446L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private int id;
+
+	@Column(name = "IsActiv")
+	private boolean aktiv;
+
+	@Column(name = "KeyFilePath")
+	private String keyfilepath;
+
+	@ManyToOne
+	@JoinColumn(name = "KUNDEN_NR")
+	private Kunde kunde;
+
+	// ++++++++++++++++++++++++++++++++++++
+	public KartenTechnologien() {
+
 	}
 
-	/** find all existing CardTechnologies with type KartenTechnologie */
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<KartenTechnologien> findKartentechnologien()
-			throws DataException {
+	// ++++++++++++++++++++++++++++++++++++
+	// Getter and Setter
+	// ++++++++++++++++++++++++++++++++++++
 
-		Query q = entityManager.createNamedQuery("findKartentechnologien");
-		return (List<KartenTechnologien>) q.getResultList();
+	public int getId() {
+		return id;
+	}
+
+	public boolean isAktiv() {
+		return aktiv;
+	}
+
+	public void setAktiv(boolean aktiv) {
+		this.aktiv = aktiv;
+	}
+
+	public String getKeyfilepath() {
+		return keyfilepath;
+	}
+
+	public void setKeyfilepath(String keyfilepath) {
+		this.keyfilepath = keyfilepath;
+	}
+
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + ", activ=" + aktiv + ",KeyfilePath=" + keyfilepath;
 	}
 }
