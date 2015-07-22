1bdb562d3c40c1ab2dcc458b914dedee6f0fabed
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-22 08:34:48 +0200

    Update ClassicDao.java

--- src/main/java/org.intercard.template.data.db.dao.impl/ClassicDao.java
+++ src/main/java/org.intercard.template.data.db.dao.impl/ClassicDao.java
@@ -1,48 +1,132 @@
-package org.intercard.template.data.db.dao.impl;
+package org.intercard.template.data.db.domain;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-import javax.persistence.Query;
+import org.intercard.template.data.db.domain.enums.Aufbautype;
 
-import org.intercard.template.data.db.dao.IClassicDao;
-import org.intercard.template.data.db.domain.Classic;
-import org.intercard.template.data.db.ex.DataException;
+/**
+ * 
+ * @author vanessa zimmermann
+ *
+ */
+@Entity
+@Table(name = "Classic")
+@NamedQueries({
 
-public class ClassicDao extends AbstractDao<Classic> implements IClassicDao {
+		@NamedQuery(name = "findActivClassic", query = "SELECT c FROM Classic c where c.aktiv= :activ"),
+		/*
+		 * @NamedQuery(name = "findClassicbySystemnummer", query =
+		 * "SELECT c FROM Classic c where c.N1V= :n1v and c.N1A = :n1a and c.n2= : n2 and c.n3= :n3  "
+		 * ),
+		 */
+		@NamedQuery(name = "findAllClassic", query = "SELECT c FROM Classic c ") })
+public class Classic extends KartenTechnologien {
 
-	@Override
-	protected Class<Classic> getDomainClass() {
-		return Classic.class;
+	/**
+	 * 
+	 */
+	@Transient
+	private static final long serialVersionUID = -35476857446L;
+
+	@Column(name = "type")
+	@Enumerated(EnumType.STRING)
+	private Aufbautype type;
+
+	@Column(name = "N1V")
+	private Integer N1V;
+
+	@Column(name = "N1A")
+	private Integer N1A;
+
+	@Column(name = "N2")
+	private Integer N2;
+
+	@Column(name = "N3")
+	private Integer N3;
+
+	@Column(name = "Kartentechnologie")
+	private String kartentechnologie = "Classic";
+
+	@Column(name = "zusammengesetzt")
+	private String zusammengesetzt;
+
+	// ++++++++++++++++++++++++++++++++++++
+	public Classic() {
+
 	}
 
-	/** Find all active Classic cardtechnologies */
+	// ++++++++++++++++++++++++++++++++++++
+	// Getter and Setter
+	// ++++++++++++++++++++++++++++++++++++
 
 	@Override
-	public Classic findActivClassic(boolean aktiv) throws DataException {
-
-		Query q = entityManager.createNamedQuery("findActivClassic");
-		q.setParameter("activ", aktiv);
-
-		// GENAU einer erwartet.
-		return (Classic) q.getSingleResult();
+	public String toString() {
+		return "Classic[" + super.toString() + ", Systemnummer=" + N1V + "/"
+				+ N1A + "." + N2 + "." + N3 + "]";
 	}
 
-	// /** Find all Classic cards */
-	// @SuppressWarnings("unchecked")
-	// @Override
-	// public List<Classic> findClassicbySystemnummer() throws DataException {
-	// Query q = entityManager.createNamedQuery("findClassicbySystemnummer");
-	//
-	// // GENAU einer erwartet
-	// return (List<Classic>) q.getResultList();
-	// }
-
-	/** Find classic by N1V/N1A/N2/N3 connected to String */
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Classic> findAllClassic() throws DataException {
-		Query q = entityManager.createNamedQuery("findAllClassic");
-
-		return (List<Classic>) q.getResultList();
+	public Aufbautype getType() {
+		return type;
 	}
+
+	public void setType(Aufbautype type) {
+		this.type = type;
+	}
+
+	public Integer getN1V() {
+		return N1V;
+	}
+
+	public void setN1V(Integer n1v) {
+		N1V = n1v;
+	}
+
+	public Integer getN1A() {
+		return N1A;
+	}
+
+	public void setN1A(Integer n1a) {
+		N1A = n1a;
+	}
+
+	public Integer getN2() {
+		return N2;
+	}
+
+	public void setN2(Integer n2) {
+		N2 = n2;
+	}
+
+	public Integer getN3() {
+		return N3;
+	}
+
+	public void setN3(Integer n3) {
+		N3 = n3;
+	}
+
+	public String getKartentechnologie() {
+		return kartentechnologie;
+	}
+
+	public void setKartentechnologie(String kartentechnologie) {
+		this.kartentechnologie = kartentechnologie;
+	}
+
+	public void setZusammengesetzt() {
+
+		zusammengesetzt = N1V + "/" + N1A + "." + N2 + "." + N3;
+	}
+
+	public String getZusammengesetzt() {
+		return zusammengesetzt;
+	}
+
 }
