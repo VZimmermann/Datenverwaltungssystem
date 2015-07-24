664e5973d3fba0a135882ca34459140d9807ecfb
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-24 15:25:53 +0200

    Update SeriennummerService.java

--- src/main/java/org.intercard.template.service.impl/SeriennummerService.java
+++ src/main/java/org.intercard.template.service.impl/SeriennummerService.java
@@ -1,9 +1,10 @@
 package org.intercard.template.service.impl;
 
-import java.util.Date;
 import java.util.List;
 
+import org.intercard.template.data.db.dao.IKundeDao;
 import org.intercard.template.data.db.dao.ISerienNummerDao;
+import org.intercard.template.data.db.domain.Kunde;
 import org.intercard.template.data.db.domain.SerienNummer;
 import org.intercard.template.data.db.ex.DataException;
 import org.intercard.template.service.ISeriennummerService;
@@ -16,73 +17,96 @@ public class SeriennummerService implements ISeriennummerService {
 			.getLogger(SeriennummerService.class);
 
 	private ISerienNummerDao seriennummerDao;
+	private IKundeDao kundeDao;
 
-	@Override
-	public Long nextfreeSerienNummer(SerienNummer seriennummer) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Long lastSerienNummer(SerienNummer seriennummer) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Long defineSeriennummernrange(SerienNummer anzahl) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean reserveSeriennummernrange(SerienNummer seriennummer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<SerienNummer> getSeriennummernrangeList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean deleteSerienNummern(SerienNummer seriennummer) {
-		if (seriennummer != null && seriennummer.getSnanfang() != 0) {
+	/** reserve a Range for a Kunde or Create a new Range */
+	public SerienNummer reserveSeriennummernRange(Kunde kunde, int snanfang,
+			int snende) {
+		SerienNummer sernum = new SerienNummer();
+		if (kunde != null && snende != 0 && snanfang != 0) {
 			try {
-				seriennummerDao.doDelete(seriennummer);
-				logger.debug(" Service Delete Seriennummer");
+
+				// sernum.setAnzahl(anzahl);
+				sernum.setSnanfang(snanfang);
+				sernum.setSnende(snende);
+				sernum.setKunde(kunde);
+
+				seriennummerDao.doSave(sernum);
+				kundeDao.doUpdate(kunde);
+				logger.debug("Reservierung war erfolgreich");
+				return sernum;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht reserviert werden");
+			}
+		}
+		return null;
+	}
+
+	/** get a Seriennummer by its beginning */
+	public SerienNummer getSeriennummer(int snanfang) {
+
+		if (snanfang != 0) {
+			try {
+				SerienNummer sernum = seriennummerDao.findOne(snanfang);
+				return sernum;
+			} catch (DataException e) {
+				logger.debug("Kein Ergebnis", e);
+			}
+		}
+		return null;
+	}
+
+	/** get a List of Seriennummern */
+	@Override
+	public List<SerienNummer> getSeriennummernList() {
+		try {
+			List<SerienNummer> serlist = seriennummerDao.findAllSeriennummern();
+			if (serlist != null && !serlist.isEmpty())
+				;
+			return serlist;
+		} catch (DataException e) {
+			logger.debug("Keine Seriennummern gefunden", e);
+		}
+		return null;
+	}
+
+	/** Delete a Range */
+	@Override
+	public boolean deleteSeriennummern(SerienNummer seriennummern) {
+		if (seriennummern != null && seriennummern.getSnanfang() != 0
+				&& seriennummern.getSnende() != 0) {
+			try {
+				seriennummerDao.doDelete(seriennummern);
 				return true;
 			} catch (DataException e) {
-				logger.debug("EX", e);
+				logger.debug("Konnte nicht gelöscht werden");
 			}
 		}
 		return false;
 	}
 
-	@Override
-	public SerienNummer getSerienNummeranfang(int snanfang) {
-		// TODO Auto-generated method stub
-		return null;
+	/*
+	 * // nächste freie Seriennummer berechen private SerienNummer
+	 * nextfreeSerienNummer(int anzahl) { // Query q = Select MAX(snanfang) from
+	 * SerienNummer s // int nextserum = q.getSingleResult();
+	 * 
+	 * // Query q = EntityManagerBuilder //
+	 * .createQuery("Select Max(snanfang) from SerienNummer"); //
+	 * TypedQuery<SerienNummer> q = EntityManager.createQuery( //
+	 * "Select Max(snanfang) from SerienNUmmer s", SerienNummer.class); return
+	 * seriennummer; }
+	 */
+
+	// @Override
+	// public int lastSerienNummer(int snende) {
+	// int lastseriennummer =
+	// }
+	public ISerienNummerDao getSeriennummerDao() {
+		return seriennummerDao;
 	}
 
-	@Override
-	public SerienNummer getSerienNummerende(int snende) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public SerienNummer getKommentar(String kommentar) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public SerienNummer getVergebenam(Date vergeben) {
-		// TODO Auto-generated method stub
-		return null;
+	public void setSeriennummerDao(ISerienNummerDao seriennummerDao) {
+		this.seriennummerDao = seriennummerDao;
 	}
 
 }
