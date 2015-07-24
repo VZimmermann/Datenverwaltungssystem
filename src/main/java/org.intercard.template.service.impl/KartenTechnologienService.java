17fdc919046a1a839f326849f4bb2be3fb59be45
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-24 15:25:29 +0200

    Update KartenTechnologienService.java

--- src/main/java/org.intercard.template.service.impl/KartenTechnologienService.java
+++ src/main/java/org.intercard.template.service.impl/KartenTechnologienService.java
@@ -10,8 +10,8 @@ import org.intercard.template.data.db.domain.Classic;
 import org.intercard.template.data.db.domain.Desfire;
 import org.intercard.template.data.db.domain.KartenTechnologien;
 import org.intercard.template.data.db.domain.Legic;
-import org.intercard.template.data.db.domain.enums.Aufbautype;
 import org.intercard.template.data.db.ex.DataException;
+import org.intercard.template.data.domain.SystemNummer;
 import org.intercard.template.service.IKartenTechnologienService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,197 +26,232 @@ public class KartenTechnologienService implements IKartenTechnologienService {
 	private IClassicDao classicDao;
 	private ILegicDao legicDao;
 
+	/**
+	 * public Classic nextfreeSystemnummerClassic(SystemNummer sysNum) { // TODO
+	 * Auto-generated method stub return null; }
+	 * 
+	 * 
+	 * public Legic nextfreeSystemnummerLegic(SystemNummer sysNum) { // TODO
+	 * Auto-generated method stub return null; }
+	 * 
+	 * 
+	 * public Desfire nextfreeSystemnummerDesfire(SystemNummer sysNum) { // TODO
+	 * Auto-generated method stub return null; }
+	 */
+
+	/** Create, update, delete a Kartentechnologie with Type Desfire */
 	@Override
-	public boolean createDesfire(Desfire desfire) {
+	public Desfire createDesfire(Desfire desfire) {
 		if (desfire != null) {
 			try {
 				desfireDao.doSave(desfire);
-				logger.debug(" Service Create Desfire");
-				return true;
+				logger.debug("Service Create Desfire");
+				return desfire;
 			} catch (DataException e) {
-				logger.debug("EX", e);
+				logger.debug("Konnte nicht erstellt werden", e);
 			}
-		}
-		return false;
-
-	}
-
-	@Override
-	public boolean updateDesfire(Desfire desfire) {
-		if (desfire != null && desfire.getId() != 0) {
-			try {
-				desfireDao.doUpdate(desfire);
-				logger.debug(" Service Update Desfire");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-
-	}
-
-	@Override
-	public boolean deleteDesfire(Desfire desfire) {
-		if (desfire != null && desfire.getId() != 0) {
-			try {
-				desfireDao.doDelete(desfire);
-				logger.debug(" Service Delete Desfire");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean createClassic(Classic classic) {
-		if (classic != null) {
-			try {
-				classicDao.doSave(classic);
-				logger.debug(" Service Create Classic");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-
-	}
-
-	@Override
-	public boolean updateClassic(Classic classic) {
-		if (classic != null && classic.getId() != 0) {
-			try {
-				classicDao.doUpdate(classic);
-				logger.debug(" Service Update Classic");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean deleteClassic(Classic classic) {
-		if (classic != null && classic.getId() != 0) {
-			try {
-				classicDao.doDelete(classic);
-				logger.debug(" Service Delete Classic");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean createLegic(Legic legic) {
-		if (legic != null) {
-			try {
-				legicDao.doSave(legic);
-				logger.debug(" Service Create Legic");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean updateLegic(Legic legic) {
-		if (legic != null && legic.getId() != 0) {
-			try {
-				legicDao.doUpdate(legic);
-				logger.debug(" Service Update Legic");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean deleteLegic(Legic legic) {
-		if (legic != null && legic.getId() != 0) {
-			try {
-				legicDao.doDelete(legic);
-				logger.debug(" Service Delete Legic");
-				return true;
-			} catch (DataException e) {
-				logger.debug("EX", e);
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public List<KartenTechnologien> getKartentechnologienList() {
-		try {
-			List<KartenTechnologien> ret = kartentechnologienDao.findAll();
-			if (ret != null && !ret.isEmpty())
-				return ret;
-		} catch (DataException e) {
-			logger.debug("EX", e);
 		}
 		return null;
 	}
 
-	//
-	// @Override
-	// public List<Aufbautype> getAufbautypeList() {
-	// List<Aufbautype> auf = classicDao.findAll();
-	// if (auf!= null && !auf.isEmpty())
-	// return auf;
-	// }
-	// catch (DataException e) {
-	// logger.debug("EX", e);
-	// }
-	// return null;
-	// }
-
 	@Override
-	public boolean getAufbaustruktur(Desfire aufbaustruktur) {
-
-		return false;
+	public Desfire updateDesfire(Desfire desfire) {
+		if (desfire != null && desfire.getId() != 0) {
+			try {
+				desfireDao.doUpdate(desfire);
+				logger.debug("Service Update Desfire");
+				return desfire;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht upgedated werden", e);
+			}
+		}
+		return null;
 	}
 
 	@Override
-	public boolean getMinorversion(Desfire minorversion) {
+	public Desfire deleteDesfire(Desfire desfire) {
+		if (desfire != null && desfire.getId() != 0) {
+			try {
+				desfireDao.doDelete(desfire);
+				logger.debug("Service Delete Desfire");
+				return desfire;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht gelöscht werden", e);
+			}
+		}
+		return null;
+	}
 
-		return false;
+	/** Create, update, delete a Kartentechnologie with Type Classic */
+	@Override
+	public Classic createClassic(Classic classic) {
+		if (classic != null) {
+			try {
+				classicDao.doSave(classic);
+				logger.debug("Service Create Classic");
+				return classic;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht erstellt werden", e);
+			}
+		}
+		return null;
 	}
 
 	@Override
-	public boolean getAufbautype(Classic aufbautype) {
+	public Classic updateClassic(Classic classic) {
+		if (classic != null && classic.getId() != 0) {
+			try {
+				classicDao.doUpdate(classic);
+				logger.debug("Service Update Classic");
+				return classic;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht upgedated werden", e);
+			}
+		}
+		return null;
+	}
 
-		return false;
+	@Override
+	public Classic deleteClassic(Classic classic) {
+		if (classic != null && classic.getId() != 0) {
+			try {
+				classicDao.doDelete(classic);
+				logger.debug("Service Delete Classic");
+				return classic;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht gelöscht werden", e);
+			}
+		}
+		return null;
+	}
+
+	/** Create, update, delete a Kartentechnologie with Type Legic */
+	@Override
+	public Legic createLegic(Legic legic) {
+		if (legic != null) {
+			try {
+				legicDao.doSave(legic);
+				logger.debug("Service Create Legic");
+				return legic;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht erstellt werden", e);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Legic updateLegic(Legic legic) {
+		if (legic != null && legic.getId() != 0) {
+			try {
+				legicDao.doUpdate(legic);
+				logger.debug("Service Update Legic");
+				return legic;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht upgedated werden");
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Legic deleteLegic(Legic legic) {
+		if (legic != null && legic.getId() != 0) {
+			try {
+				legicDao.doDelete(legic);
+				logger.debug("Service Delete Legic");
+				return legic;
+			} catch (DataException e) {
+				logger.debug("Konnte nicht gelöscht werden");
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * nach einer Systemnummer suchen und wenn es diese in mehreren
+	 * Kartentechnologien gibt ausgeben
+	 */
+	@Override
+	public List<KartenTechnologien> getKartentechnologienList(
+			SystemNummer systemnummer) {
+		try {
+			List<KartenTechnologien> kartentechnologien = kartentechnologienDao
+					.findAllKartentechnologien();
+			if (kartentechnologien != null && !kartentechnologien.isEmpty())
+				return kartentechnologien;
+		} catch (DataException e) {
+			logger.debug("Keine Kartentechnologien vorhanden", e);
+		}
+		return null;
 	}
 
 	@Override
 	public KartenTechnologien getKartenTechnologien(int id) {
+		if (id != 0) {
+			try {
+				KartenTechnologien kartid = kartentechnologienDao.findOne(id);
+				return kartid;
+			} catch (DataException e) {
+				logger.debug("Keine Ergebnisse", e);
+			}
+		}
+		return null;
+	}
+
+	/** get only one Systemnummer */
+	@Override
+	public Classic searchClassicSystemnummer(SystemNummer systemnummer) {
+		if (systemnummer != null) {
+			/* ??????????????????????????????????????????????? */
+
+		}
+		return null;
+	}
+
+	@Override
+	public Legic searchLegicSystemnummer(SystemNummer systemnummer) {
 
 		return null;
 	}
 
 	@Override
-	public List<Aufbautype> getAufbautypeList() {
+	public Desfire searchDesfireSystemnummer(SystemNummer systemnummer) {
 
 		return null;
 	}
 
-	/*
-	 * SETTER FOR SPRING CONFIG
-	 */
+	public IKartenTechnologienDao getKartentechnologienDao() {
+		return kartentechnologienDao;
+	}
+
+	public IDesfireDao getDesfireDao() {
+		return desfireDao;
+	}
+
+	public IClassicDao getClassicDao() {
+		return classicDao;
+	}
+
+	public ILegicDao getLegicDao() {
+		return legicDao;
+	}
 
 	public void setKartentechnologienDao(
 			IKartenTechnologienDao kartentechnologienDao) {
 		this.kartentechnologienDao = kartentechnologienDao;
+	}
+
+	public void setDesfireDao(IDesfireDao desfireDao) {
+		this.desfireDao = desfireDao;
+	}
+
+	public void setClassicDao(IClassicDao classicDao) {
+		this.classicDao = classicDao;
+	}
+
+	public void setLegicDao(ILegicDao legicDao) {
+		this.legicDao = legicDao;
 	}
 
 }
