8dbd0f89a6c882f357f73e3d836ef46dec92a874
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-24 15:26:43 +0200

    Update IExportService.java

--- src/main/java/org.intercard.template.service/IExportService.java
+++ src/main/java/org.intercard.template.service/IExportService.java
@@ -7,6 +7,5 @@ public interface IExportService {
 	// Exportpfad erzeugen-->Sysnr_Export_SAPNrxxx
 	public String createExportpfad();
 
-	// SAPnummer auslesen
-	public boolean getSAPnr(Kunde sapnummer);
+	
 }
