282792291c0545a2256f888220352cda54e2589c
VZimmermann <VZimmermann@users.noreply.github.com>
2015-07-24 15:26:06 +0200

    Update VerbundService.java

--- src/main/java/org.intercard.template.service.impl/VerbundService.java
+++ src/main/java/org.intercard.template.service.impl/VerbundService.java
@@ -3,7 +3,6 @@ package org.intercard.template.service.impl;
 import java.util.List;
 
 import org.intercard.template.data.db.dao.IVerbundDao;
-import org.intercard.template.data.db.domain.Kunde;
 import org.intercard.template.data.db.domain.Verbund;
 import org.intercard.template.data.db.ex.DataException;
 import org.intercard.template.service.IVerbundService;
@@ -17,87 +16,98 @@ public class VerbundService implements IVerbundService {
 
 	private IVerbundDao verbundDao;
 
+	/** get a List with all excisting Verbünde */
 	@Override
 	public List<Verbund> getVerbundList() {
-		// TODO Auto-generated method stub
+		try {
+			List<Verbund> verbundlist = verbundDao.findAllVerbund();
+			if (verbundlist != null && !verbundlist.isEmpty())
+				return verbundlist;
+		} catch (DataException e) {
+			logger.debug("Keine Ergenisse");
+		}
 		return null;
 	}
 
+	/** get a Verbund by searching for a name */
 	@Override
-	public boolean createVerbund(Verbund verbund) {
+	public Verbund getVerbund(String name) {
+		if (name != null) {
+			try {
+				Verbund verbund = verbundDao.findVerbund(name);
+				return verbund;
+			} catch (DataException e) {
+				logger.debug("Kein Verbund gefunden");
+			}
+		}
+		return null;
+	}
+
+	/** get a Verbund by searching for an id */
+	@Override
+	public Verbund getVerbund(int id) {
+		if (id != 0) {
+			try {
+				Verbund verbund = verbundDao.findOne(id);
+				return verbund;
+			} catch (DataException e) {
+				logger.debug("Kein Verbund gefunden");
+			}
+		}
+		return null;
+	}
+
+	/** Create a new Verbund */
+	@Override
+	public Verbund createVerbund(Verbund verbund) {
 		if (verbund != null) {
 			try {
 				verbundDao.doSave(verbund);
-				logger.debug(" Service Create Verbund");
-				return true;
+				logger.debug("Service Create Verbund");
+				return verbund;
 			} catch (DataException e) {
-				logger.debug("EX", e);
+				logger.debug("Konnte nicht erstellt werden", e);
 			}
 		}
-		return false;
+		return null;
+
 	}
 
+	/** Update an excisting Verbund */
 	@Override
-	public boolean updateVerbund(Verbund verbund) {
+	public Verbund updateVerbund(Verbund verbund) {
 		if (verbund != null && verbund.getId() != 0) {
 			try {
 				verbundDao.doUpdate(verbund);
-				logger.debug(" Service Update Verbund");
-				return true;
+				logger.debug("Service Update Verbund");
+				return verbund;
 			} catch (DataException e) {
-				logger.debug("EX", e);
+				logger.debug("Konnte nicht upgedated werden", e);
 			}
 		}
-		return false;
+		return null;
+
 	}
 
+	/** Delete an excisting Verbund */
 	@Override
-	public boolean deleteVerbund(Verbund verbund) {
+	public Verbund deleteVerbund(Verbund verbund) {
 		if (verbund != null && verbund.getId() != 0) {
 			try {
 				verbundDao.doDelete(verbund);
-				logger.debug(" Service Delete Verbund");
-				return true;
+				logger.debug("Service Delete Verbund");
+				return verbund;
 			} catch (DataException e) {
-				logger.debug("EX", e);
+				logger.debug("Konnte nicht gelöscht werden");
 			}
 		}
-		return false;
-	}
-
-	@Override
-	public List<Verbund> getVerbundschluesselList() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Verbund getVerbund(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public IVerbundDao getVerbundDao() {
+		return verbundDao;
 	}
 
-	@Override
-	public Verbund getVerbund(String verbundname) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Verbund> searchVerbund(String verbund) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Kunde> getVerbundsTeilnehmer(Verbund verbund) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/*
-	 * SETTER FOR SPRING CONFIG
-	 */
 	public void setVerbundDao(IVerbundDao verbundDao) {
 		this.verbundDao = verbundDao;
 	}
